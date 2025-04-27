7517e32ebb5b35a9831e2dcef41905e4de5581c1
Felipe Ferreira <felipe.w.ferreira@pwc.com>
2025-04-27 10:48:39 -0400

    chore: removes Dometrain.Movies.Contracts refs

--- src/shared/Dometrain.Movies.Contracts/Requests/CreateMovieRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dometrain.Movies.Contracts;
-
-public class CreateMovieRequest
-{
-    public required string Title { get; init; }
-    public required int YearOfRelease { get; init; }
-    public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
-}
--- src/shared/Dometrain.Movies.Contracts/Response/MoviesResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Dometrain.Movies.Contracts.Response;
-
-public class MoviesResponse
-{
-    public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
-}
