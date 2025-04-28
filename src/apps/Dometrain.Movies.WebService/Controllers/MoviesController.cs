5b1d8aa21cc7c8c79f99ec5355b88136777efeb7
Felipe Ferreira <felipe.w.ferreira@pwc.com>
2025-04-28 00:32:38 -0400

    chore: updating moving away from minimal apis

---
+++ src/apps/Dometrain.Movies.WebService/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Dometrain.Movies.ApplicationAbstractions;
+using Dometrain.Movies.WebService.Contracts.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dometrain.Movies.WebService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllMoviesAsync(IMoviesRepository moviesRepository, CancellationToken ct = default)
+        {
+            var allMovies = await moviesRepository.GetAllAsync(ct);
+            var moviesResponse = new MoviesResponse
+            {
+                Items = allMovies.Select(movie => new MovieResponse
+                {
+                    Genres = movie.Genres,
+                    Id = movie.Id,
+                    Title = movie.Title,
+                    YearOfRelease = movie.YearOfRelease,
+                })
+            };
+            return Ok(moviesResponse);
+        }
+    }
+}
